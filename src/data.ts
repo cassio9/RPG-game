dafbd410b35fd0d7e942a694a5b9d802126c7c18
Cassio Soares <cassiosoares9@hotmail.com>
2022-08-30 23:32:44 +0900

    responsive design

--- src/data.ts
+++ src/data.ts
@@ -1,28 +1,28 @@
 const characterData = {
   batman: {
     name: "Batman",
-    avatar: "./src/img/legoBatman.jpg",
+    avatar: "./img/legoBatman.jpg",
     health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
   spaceGuy: {
     name: "Space Trooper",
-    avatar: "./src/img/spaceGuy.jpg",
+    avatar: "./img/spaceGuy.jpg",
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
   },
   rabbit: {
     name: "Crazy Rabbit",
-    avatar: "./src/img/scaryRabbit.jpg",
+    avatar: "./img/scaryRabbit.jpg",
     health: 30,
     diceCount: 2,
     currentDiceScore: [],
   },
   darth: {
     name: "Darth Vader",
-    avatar: "./src/img/darthVader.jpg",
+    avatar: "./img/darthVader.jpg",
     health: 20,
     diceCount: 3,
     currentDiceScore: [],
